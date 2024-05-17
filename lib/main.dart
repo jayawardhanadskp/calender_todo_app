534a0cc3283b4976395a5eb2a9974edef2f51c65
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-05-17 23:58:46 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 import 'screens/home_screen.dart';
 
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // initialize timezone package
+  tz.initializeTimeZones();
+  tz.setLocalLocation(tz.getLocation('Asia/Colombo'));
+
+  // initialize notifications for android
+  AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  // for ios
+  final DarwinInitializationSettings initializationSettingsIOS = const DarwinInitializationSettings(
+    requestAlertPermission: true,
+    requestBadgePermission: true,
+    requestSoundPermission: true,
+  );
+
+  // initialize settings
+  final InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsIOS,
+  );
+
+  // plugin
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calender App',
+      title: 'Calendar App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade100),
         useMaterial3: true,
-
       ),
-
-
 
       home:
       // splash screen
       AnimatedSplashScreen(
-          duration: 3000,
-          splash: Icons.calendar_month_outlined, splashIconSize: 100,
-          nextScreen: HomePage(),
-          splashTransition: SplashTransition.fadeTransition,
-          backgroundColor: Colors.blue.shade100),
-          color: Colors.white,
+        duration: 3000,
+        splash: Icons.calendar_month_outlined,
+        splashIconSize: 100,
+        nextScreen: const HomePage(),
+        splashTransition: SplashTransition.scaleTransition,
+        backgroundColor: Colors.blue.shade100,
+      ),
     );
   }
 }
+
 
